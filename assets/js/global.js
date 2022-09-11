d28ad24c3422424e9700f06958e4409d9e8f63e0
Zulqs <zulqs@apps.ipb.ac.id>
2022-09-12 00:45:17 +0700

    Adding Registration form

--- assets/js/global.js
+++ assets/js/global.js
@@ -3,3 +3,23 @@ const phone_navbar = document.querySelector('.phone_navbar');
 phone_navbar.addEventListener('click', function () {
     this.classList.toggle('is-active');
 });
+
+var modalbox = document.getElementById("regisbox");
+
+var btn = document.getElementById("gabung");
+
+var span = document.getElementsByClassName("tutup")[0];
+
+btn.onclick = function () {
+    modalbox.style.display = "block";
+}
+
+span.onclick = function() {
+    modalbox.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modalbox) {
+        modalbox.style.display = "none";
+    }
+}
