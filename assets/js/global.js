5b0bc7856dd18b18ddb36fdb4f2216369f8b46d8
Zulqs <zulqs@apps.ipb.ac.id>
2022-09-13 23:08:41 +0700

    +register modalbox animation

--- assets/js/global.js
+++ assets/js/global.js
@@ -4,23 +4,20 @@ phone_navbar.addEventListener('click', function () {
     this.classList.toggle('is-active');
 });
 
- var modalbox = document.getElementById("regisbox");
+var modalbox = document.getElementById('regisbox');
 
- var btn = document.getElementById("gabung");
+var btn = document.getElementById("gabung");
 
- var span = document.getElementsByClassName("tutup")[0];
+var span = document.getElementsByClassName("tutup")[0];
 
- btn.onclick = function () {
-     modalbox.style.display = "block";
- }
+btn.onclick = function() {
+    modalbox.classList.add('show');
+}
 
- span.onclick = function() {
-     modalbox.style.display = "none";
- }
-
- window.onclick = function(event) {
-     if (event.target == modalbox) {
-         modalbox.style.display = "none";
-     }
- }
-
+span.onclick = function() {
+    modalbox.classList.add('out');
+    modalbox.classList.remove('show');
+    setTimeout(function(){
+        modalbox.classList.remove('out');
+    }, 1000);
+}
